a436b7ff3a7a0892f3609efad96de154839ff2d9
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-07-11 20:49:35 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ cd build-scripts
 cmake $RECIPE_DIR/scripts
 cd ..
 
-# autoreconf -i
+autoreconf --force --install
 ./configure --prefix=$PREFIX FCFLAGS="-O2 -std=legacy" CFLAGS="-O2"
 
 make -j$(nproc)
